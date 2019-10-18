dbee8b02a191a509b921b2f7e5f1596e78e764ad
dhardik <35969168+dhardik@users.noreply.github.com>
2019-10-18 12:22:42 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,66 @@
+import ds.linkedlist.*;
+import ds.stack.*;
+import ds.tree.*;
+
+public class Demo {
+  public static void main(String[] args) {
+    // SinglyLinkedList sll = new SinglyLinkedList();
+    // sll.addFirst(1);
+    // sll.addLast(2);
+    // sll.addLast(3);
+    // sll.addLast(4);
+    // sll.addLast(5);
+    // sll.addLast(6);
+    // System.out.println(sll.findNthFromEnd(6));
+    // sll.reverseLinkedList();
+    // System.out.println(sll.containsLoop());
+    // DoublyLinkedList dll = new DoublyLinkedList();
+    // System.out.println(dll.size());
+    // CircularLinkedList cll = new CircularLinkedList();
+    // cll.addFirst(5);
+    // System.out.println(cll.first());
+
+    // ArrayStack as = new ArrayStack(5);
+    // for(int i=1;i<=5;i++) {
+    //   as.push(i*3);
+    // }
+    // for(int i=1;i<=5;i++) {
+    //   System.out.println(as.top());
+    //   as.pop();
+    // }
+
+    // BinarySearchTree root=new BinarySearchTree();
+		// root.insert(4);
+		// root.insert(3);
+		// root.insert(1);
+		// root.insert(2);
+		// root.insert(9);
+		// root.insert(6);
+		// root.insert(5);
+		// root.insert(7);
+		// root.insert(8);
+		// System.out.println("Inorder:-");
+		// root.inOrder();
+		// System.out.println("Height :-");
+		// System.out.println(root.height());
+		// System.out.println("Paths:-");
+		// root.printPaths();
+    // System.out.println("Level Order");
+		// root.printLevelOrder();
+		// System.out.println("Reverse Level Order");
+		// root.printRevLevelOrder();
+		// System.out.println("mirror");
+		// root.mirror();
+		// root.inOrder();
+		// root.mirror();
+		// System.out.println("Tree is BST :- "+root.isBST());
+		// System.out.println("Diameter = "+root.diameter());
+		// System.out.println("Vertical Order :- ");
+		// root.verticalOrder();
+		// System.out.println("After Deleting 5");
+		// root.deleteKey(5);
+		// root.inOrder();
+		// System.out.println("lsa of 3&9 is"+root.lca(3,9));
+		// root.delete();
+  }
+}
